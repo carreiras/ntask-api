d78e47b3530e540fa167354ad314403c0aa22ec9
Ewerton Carreira <ewertoncarreira@gmail.com>
2021-10-25 12:52:59 -0300

    primeiro endpoint

--- index.js
+++ index.js
@@ -2,8 +2,23 @@ const express = require("express");
 const PORT = 3000;
 const app = express();
 
+app.set("json spaces", 4);
+
 app.get("/", (req, res) => {
   res.json({ status: "NTask API" });
+});
+
+app.get("/tasks", (req, res) => {
+  res.json({
+    tasks: [
+      {
+        title: "Fazer compras",
+      },
+      {
+        title: "Consertar p pc",
+      },
+    ],
+  });
 });
 
 app.listen(PORT, () => {
