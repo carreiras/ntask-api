e30d3840325d80040f141384ae8cba57fd4905c9
Ewerton Carreira <ewertoncarreira@gmail.com>
2021-10-26 15:56:05 -0300

    organizando as rotas das terefas

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,12 +1,28 @@
 module.exports = app => {
     const Tasks = app.models.tasks;
 
-    app.get('/tasks', async (req, res) => {
-        try {
-            const tasks = await Tasks.findAll();
-            res.json({tasks});
-        } catch (err) {
-            res.status(500).json(err);
-        }
-    });
+    app.route('/tasks')
+            .all((req, res) => {
+                // middleware de pré-execução das rotas
+            })
+            .get((req, res) => {
+                // "/tasks: lista tarefas
+            })
+            .post((req, res) => {
+                // "/tasks: cadastra uma nova tarefa
+            });
+
+    app.route('/tasks/:id')
+            .all((req, res) => {
+                // Middleware de pré-execução das rotas
+            })
+            .get((req, res) => {
+                // "/tasks/1: consulta uma tarefa
+            })
+            .put((req, res) => {
+                // "/tasks/1: atualiza uma tarefa
+            })
+            .delete((req, res) => {
+                // "/tasks/1: exclui uma tarefa
+            });
 };
