407f44f1ae7b9300c0fe64b92242f75c14ad9d19
Ewerton Carreira <ewertoncarreira@gmail.com>
2021-10-27 11:39:11 -0300

    consultando uma tarefa via método get

--- routes/tasks.js
+++ routes/tasks.js
@@ -19,18 +19,27 @@ module.exports = app => {
                     const result = await Tasks.create(req.body);
                     res.json(result);
                 } catch (err) {
-                    res.tatus(412).json({msg: err.message});
+                    res.status(412).json({msg: err.message});
                 }
             });
 
     app.route('/tasks/:id')
             .all((req, res) => {
-                // Middleware de pré-execução das rotas
                 delete req.body.id;
                 next();
             })
-            .get((req, res) => {
-                // "/tasks/1: consulta uma tarefa
+            .get(async (req, res) => {
+                try {
+                    const {id} = req.params;
+                    const where = {id};
+                    const result = await Tasks.findOne({where});
+                    if (result)
+                        res.json(result);
+                    else
+                        res.sendStatus(404);
+                } catch (err) {
+                    res.status(412).json({msg: err.message});
+                }
             })
             .put((req, res) => {
                 // "/tasks/1: atualiza uma tarefa
