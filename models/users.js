309976d83d30c9266a989bf7bce1315d3fe6decc
Ewerton Carreira <ewertoncarreira@gmail.com>
2021-11-08 15:45:09 -0300

    gerando tokens para usuários autenticados

--- models/users.js
+++ models/users.js
@@ -1,4 +1,5 @@
-const { DataTypes } = require("sequelize");
+const bcrypt = require('bcrypt');
+const {DataTypes} = require("sequelize");
 
 module.exports = (app) => {
     const Users = app.db.define("Users", {
@@ -29,6 +30,19 @@ module.exports = (app) => {
                 notEmpty: true,
             },
         },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notEmpty: true,
+            },
+            set(value) {
+                const salt = bcrypt.genSaltSync();
+                const password = bcrypt.hashSync(value, salt);
+                this.setDataValue('password', password);
+            }
+        },
     });
+
     return Users;
 };
--- routes/tasks.js
+++ routes/tasks.js
@@ -2,55 +2,59 @@ module.exports = app => {
     const Tasks = app.models.tasks;
 
     app.route('/tasks')
+        .all(app.auth.authenticate())
         .get(async (req, res) => {
             try {
-                const result = await Tasks.findAll();
+                const where = {userId: req.user.id};
+                const result = await Tasks.findAll({where});
                 res.json(result);
             } catch (err) {
-                res.status(412).json({ msg: err.message });
+                res.status(412).json({msg: err.message});
             }
         })
         .post(async (req, res) => {
             try {
+                req.body.userId = req.iser.id;
                 const result = await Tasks.create(req.body);
                 res.json(result);
             } catch (err) {
-                res.status(412).json({ msg: err.message });
+                res.status(412).json({msg: err.message});
             }
         });
 
     app.route('/tasks/:id')
+        .all(app.auth.authenticate())
         .get(async (req, res) => {
             try {
-                const { id } = req.params;
-                const where = { id };
-                const result = await Tasks.findOne({ where });
+                const {id} = req.params;
+                const where = {id, userId: req.user.id};
+                const result = await Tasks.findOne({where});
                 if (result)
                     res.json(result);
                 else
                     res.sendStatus(404);
             } catch (err) {
-                res.status(412).json({ msg: err.message });
+                res.status(412).json({msg: err.message});
             }
         })
         .put(async (req, res) => {
             try {
-                const { id } = req.params;
-                const where = { id };
-                await Tasks.update(req.body, { where });
+                const {id} = req.params;
+                const where = {id, userId: req.user.id};
+                await Tasks.update(req.body, {where});
                 res.sendStatus(204);
             } catch (err) {
-                res.status(412).json({ msg: err.message });
+                res.status(412).json({msg: err.message});
             }
         })
         .delete(async (req, res) => {
             try {
-                const { id } = req.params;
-                const where = { id };
-                await Tasks.destroy( { where });
+                const {id} = req.params;
+                const where = {id, userId: req.user.id};
+                await Tasks.destroy({where});
                 res.sendStatus(204);
             } catch (err) {
-                res.status(412).json({ msg: err.message });
+                res.status(412).json({msg: err.message});
             }
         });
 };
