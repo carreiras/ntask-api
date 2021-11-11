6d4a1681afcb56e334276a6bccd07ba34e54d432
Ewerton Carreira <ewertoncarreira@gmail.com>
2021-11-11 17:55:34 -0300

    configurando embiente para testes

--- index.js
+++ index.js
@@ -7,12 +7,14 @@ const app = express();
 app.set('json spaces', 4);
 
 consign()
-        .include('db.js')
-        .then('models')
-        .then('associations.js')
-        .then('auth.js')
-        .then('middlewares.js')
-        .then('routes')
-        .then('boot.js')
-        .into(app);
+    .include('db.js')
+    .then('models')
+    .then('associations.js')
+    .then('auth.js')
+    .then('middlewares.js')
+    .then('routes')
+    .then('boot.js')
+    .into(app);
+
+module.exports = app;
 
